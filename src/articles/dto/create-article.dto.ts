c59f0a741abfba762f05fe6236e07c6f4bff253b
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2024-12-15 20:15:29 +0700

    error handling

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class CreateArticleDto {
   @IsString()
